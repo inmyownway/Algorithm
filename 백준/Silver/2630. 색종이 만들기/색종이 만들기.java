a222717d94e930548cd60cb7464cc53a1ae4ebc4
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-01-30 01:08:11 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 13304 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    static int N;
+    static int[][] arr;
+    static boolean[][] v;
+    static int white;
+    static int black;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N= Integer.parseInt(bf.readLine());
+
+        StringTokenizer st;
+        arr=new int[N][N];
+        v=new boolean[N][N];
+        for(int i=0;i<N;i++)
+        {
+            st=new StringTokenizer(bf.readLine());
+            for(int j=0;j<N;j++)
+            {
+                arr[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        divide(0,0,N);
+        System.out.println(white);
+        System.out.println(black);
+        }
+
+        public static void divide(int x,int y,int n)
+        {
+            boolean flag= true;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(arr[x][y]!=arr[x+i][y+j])
+                    {
+                        flag=false;
+                        break;
+                    }
+                    if(flag==false)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(flag)
+            {
+                if(arr[x][y]==0) white++;
+                else black++;
+            }
+            else
+            {
+                divide(x+n/2,y+n/2,n/2);
+                divide(x+n/2,y,n/2);
+                divide(x,y+n/2,n/2);
+                divide(x,y,n/2);
+
+            }
+        }
+    }
