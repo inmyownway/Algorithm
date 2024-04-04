27d703d4d03a7c83d749d7905d9ba60f1558278a
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-04-04 09:53:32 +0900

    [Gold IV] Title: 키 순서, Time: 992 ms, Memory: 47304 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		
+		
+	st= new StringTokenizer(bf.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M= Integer.parseInt(st.nextToken());
+			
+			int[][] board = new int[N][N];
+			int[][] up = new int[N][N];
+			int[] l =new int[N];
+			for(int i=0;i<M;i++)
+			{
+				st= new StringTokenizer(bf.readLine());
+				
+				int a= Integer.parseInt(st.nextToken());
+				int b= Integer.parseInt(st.nextToken());
+				
+				board[b-1][a-1]=1;
+				up[a-1][b-1]=1;
+				
+			}
+			
+		
+	
+			for(int i=0;i<N;i++)
+			{
+				boolean[] v = new boolean[N];
+				v[i]=true;
+				
+				Queue<Integer> q= new LinkedList<>();
+				q.add(i);
+				int cnt=0;
+				while(!q.isEmpty())
+				{
+					
+					int current = q.poll();
+					for(int j=0;j<N;j++)
+					{
+						if(board[current][j]==1 && v[j]==false)
+						{
+							q.add(j);
+							cnt++;
+							v[j]=true;
+							
+						}
+					}
+				}
+				l[i]=cnt;
+			}
+			
+			
+			for(int i=0;i<N;i++)
+			{
+				boolean[] v = new boolean[N];
+				v[i]=true;
+				
+				Queue<Integer> q= new LinkedList<>();
+				q.add(i);
+				int cnt=0;
+				while(!q.isEmpty())
+				{
+					
+					int current = q.poll();
+					for(int j=0;j<N;j++)
+					{
+						if(up[current][j]==1 && v[j]==false)
+						{
+							q.add(j);
+							cnt++;
+							v[j]=true;
+							
+						}
+					}
+				}
+				l[i]+=cnt;
+			}
+
+			int answer=0;
+			for(int i=0;i<N;i++)
+			{
+				if(l[i]==N-1)
+					answer++;
+			}
+			
+			System.out.println(answer);
+		}
+
+	
+
+}
