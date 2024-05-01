7827f689d51b11bb9e4523ffa59ed01aaea38372
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-05-01 20:50:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 190 ms, Memory: 48,012 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,184 +1,219 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Solution {
-
     static int N,M,C;
     static int[][] board;
     static boolean[][] visited;
-    static boolean[] isVisited;
-    static int dMax;
+    static ArrayList<Integer> first,second;
     static int answer;
+    static int f,s;
+    static boolean[] tempV;
+
+
+
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int testCase = Integer.parseInt(bf.readLine());
+        int testCase= Integer.parseInt(bf.readLine());
 
-        for (int tc = 1; tc < testCase + 1; tc++) {
-            st = new StringTokenizer(bf.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            C = Integer.parseInt(st.nextToken());
+        for(int test=1;test<testCase+1;test++)
+        {
+            st= new StringTokenizer(bf.readLine());
+            N= Integer.parseInt(st.nextToken());
+            M= Integer.parseInt(st.nextToken());
+            C= Integer.parseInt(st.nextToken());
 
-            board = new int[N][N];
-            visited = new boolean[N][N];
+            board= new int[N][N];
             answer= Integer.MIN_VALUE;
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(bf.readLine());
-                for (int j = 0; j < N; j++) {
-                    board[i][j] = Integer.parseInt(st.nextToken());
+            for(int i=0;i<N;i++)
+            {
+                st= new StringTokenizer(bf.readLine());
+                for(int j=0;j<N;j++)
+                {
+                    board[i][j]=Integer.parseInt(st.nextToken());
                 }
             }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N - M + 1; j++) {
-                    //System.out.println("i j " + i + " " + j);
-                    int[][] worker1 = new int[M][2];
+            for(int i=0;i<N;i++)
+            {
+                for(int j=0;j<=N-M;j++)
+                {
                     visited = new boolean[N][N];
 
-                    for (int idx = 0; idx < M; idx++) {
-                        worker1[idx][0] = i;
-                        worker1[idx][1] = j + idx;
-                        visited[i][j + idx] = true;
-                    }
+                    first= new ArrayList<>();
+                    //second= new ArrayList<>();
 
-                    //System.out.println("1이 선택한거");
-                    //print(worker1);
-                    //System.out.println("2 선택의수 ");
-                    for (int a = i; a < N; a++) {
-                        for (int b = 0; b < N - M + 1; b++) {
-                            //System.out.println("a b " + a + " " + b);
-                            int cnt=0;
-                            int[][] worker2 = new int[M][2];
+                    f=0;
+                    //System.out.println("first start = i,j "+ i+" "+j);
+                    fisrtGetHoney(i,j);
+                    //System.out.println(f);
+                    secondGetHoney();
 
-                            for(int idx=0 ;idx<M;idx++)
-                            {
-                                if(visited[a][b+idx] )
-                                {
-                                    break;
 
-                                }
-                                else
-
-                                {    worker2[idx][0]=a;
-                                    worker2[idx][1]=b+idx;
-                                        cnt++;
-                                }
-
-                            }
-                            if(cnt==M)
-                            {
-                                int num1 = find(worker1);
-                                int num2= find(worker2);
-
-                                if(answer<num1+num2)
-
-                                {
-                                    //System.out.println("worker1 : "+ num1);
-                                   //print(worker1);
-                                   // System.out.println("worker2 : "+num2);
-                                    //print(worker2);
-                                   // System.out.println();
-
-                                }
-                                answer=Math.max(answer,num1+num2);
-
-                            }
-
-                        }
-
-                    }
                 }
-
             }
-
-            System.out.println("#"+tc+" "+answer);
+            System.out.println("#"+test+" "+answer);
         }
+
     }
-                public static void print ( int[][] arr)
+    private static void fisrtGetHoney(int x,int y)
+    {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int idx=y;idx<y+M;idx++)
+        {
+            temp.add(board[x][idx]);
+            visited[x][idx]=true;
+        }
+
+        tempV= new boolean[temp.size()];
+        f=0;
+
+        //System.out.println("first " +f);
+        makeSubset(temp,0);
+
+    }
+    private static void secondGetHoney() {
+
+        s=0;
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<=N-M;j++)
+            {
+                second= new ArrayList<>();
+
+                boolean flag= false;
+                ArrayList<Integer> temp = new ArrayList<>();
+                for(int idx=j;idx<j+M;idx++)
                 {
-                    for (int[] a : arr) {
-                        System.out.print(Arrays.toString(a) + " ");
-
-                    }
-                    System.out.println();
-                }
-                public static void print ( boolean[][] arr)
-                {
-                    for (boolean[] a : arr) {
-                        System.out.println(Arrays.toString(a));
-
-                    }
-                    System.out.println();
-                }
-                public static int find(int[][] worker)
-                {
-
-                    int l = worker.length;
-                    Integer[] arr = new Integer[l];
-
-                    for(int i=0;i<l;i++)
+                    if(visited[i][idx])
                     {
-                        int x= worker[i][0];
-                        int y= worker[i][1];
-                        arr[i]=board[x][y];
-
+                        flag=true;
+                        break;
                     }
-                    Arrays.sort(arr, Collections.reverseOrder());
-                    //System.out.println(Arrays.toString(arr));
-                    int result=0;
-
-                    int sum=0;
-
-                    isVisited = new boolean[l];
-                    dMax = Integer.MIN_VALUE;
-
-
-                    dfs(0,l,arr);
-//                    for(int i=0;i<l;i++)
-//                    {
-//                        if(sum+arr[i]<=C)
-//                        {
-//                            sum+=arr[i];
-//                            result+=(arr[i]*arr[i]);
-//                        }
-//
-//                    }
-                    return dMax;
-
+                    temp.add(board[i][idx]);
                 }
-                public static void dfs(int depth,int R,Integer[] a)
+                if(!flag)
                 {
-                    if(depth==R)
+
+                    s=0;
+                    tempV= new boolean[temp.size()];
+                    makeSubset1(temp,0);
+                }
+
+                check();
+
+            }
+        }
+
+    }
+    public static void makeSubset(ArrayList<Integer> temp,int cnt)
+    {
+
+
+        if(cnt==temp.size())
+        {
+            int sum=0;
+
+            for(int i=0;i<tempV.length;i++)
+            {
+                if(tempV[i])
+                {
+                    //System.out.print(temp.get(i)+" ");
+                    sum+=temp.get(i);
+                }
+
+            }
+            //System.out.println();
+            int sumTemp=0;
+            if(sum<=C)
+            {
+                for(int i=0;i<tempV.length;i++)
+                {
+                    if(tempV[i])
                     {
-                        int s=0;
-                        int t=0;
-                        for(int i=0;i<depth;i++)
-                        {
-                            if (isVisited[i])
-                            {
-                                s+=a[i];
-                                t+= a[i]*a[i];
-                            }
-                        }
-
-                        if(s<=C)
-                            dMax=Math.max(dMax,t);
-                        return;
+                        sumTemp+=temp.get(i)*temp.get(i);
                     }
-
-                    isVisited[depth]=true;
-                    dfs(depth+1,R,a);
-                    isVisited[depth]=false;
-                    dfs(depth+1,R,a);
 
                 }
             }
+       //     System.out.println(f);
+            f= Integer.max(f,sumTemp);
+         //   System.out.println(f);
+
+            return;
+        }
+
+
+        tempV[cnt]=true;
+        makeSubset(temp,cnt+1);
+        tempV[cnt]=false;
+        makeSubset(temp,cnt+1);
+
+
+
+
+    }
+
+    public static void makeSubset1(ArrayList<Integer> temp,int cnt)
+    {
+
+        if(cnt==temp.size())
+        {
+            int sum=0;
+
+            for(int i=0;i<tempV.length;i++)
+            {
+                if(tempV[i])
+                {
+                    sum+=temp.get(i);
+                }
+
+            }
+            int sumTemp=0;
+            if(sum<=C)
+            {
+                for(int i=0;i<tempV.length;i++)
+                {
+                    if(tempV[i])
+                    {
+                        sumTemp+=temp.get(i)*temp.get(i);
+                    }
+
+                }
+            }
+          //  System.out.println(s);
+            s= Integer.max(s,sumTemp);
+
+            return;
+        }
+
+
+        tempV[cnt]=true;
+        makeSubset1(temp,cnt+1);
+        tempV[cnt]=false;
+        makeSubset1(temp,cnt+1);
+
+
+
+
+    }
+
+
+
+
+    public static void check()
+    {
+
+
+        answer= Integer.max(answer,f+s);
+    }
+}
