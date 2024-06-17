34a2e88f66ab2618d28c99ec21fcadcd6d0d64e4
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-06-17 11:47:35 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 76 ms, Memory: 77864 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, K;
+    static char[][] arr;
+    static boolean[][][] v;
+    static String answer;
+    static int cnt;
+    static int maxLen = Integer.MIN_VALUE;
+    static Map<String, Integer> map;
+    static int[] dx = {0, 0, -1, 1, 1, 1, -1, -1};
+    static int[] dy = {1, -1, 0, 0, 1, -1, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = bf.readLine();
+
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        map = new HashMap<>();
+
+        for (int i = 0; i < K; i++) {
+            answer = bf.readLine();
+
+            map.put(answer, 0);
+
+            maxLen = Math.max(maxLen, answer.length());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                find(i, j, "" + arr[i][j]);
+            }
+        }
+
+        Collection<Integer> value = map.values();
+        for (Integer v : value) {
+            System.out.println(v);
+        }
+    }
+
+    public static void find(int x, int y, String words) {
+
+        if (map.get(words) != null) {
+
+            map.put(words, map.get(words) + 1);
+
+        }
+        if (words.length() == maxLen) {
+            return;
+        }
+
+        for (int d = 0; d < 8; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            nx = checkX(nx);
+            ny = checkY(ny);
+
+            find(nx, ny, words + arr[nx][ny]);
+
+
+        }
+
+
+    }
+
+    public static int checkX(int x) {
+        if (x >= N) {
+            return 0;
+        } else if (x == -1) {
+            return N - 1;
+        }
+
+        return x;
+    }
+
+    public static int checkY(int x) {
+        if (x >= M) {
+            return 0;
+        } else if (x == -1) {
+            return M - 1;
+        }
+
+        return x;
+    }
+}
