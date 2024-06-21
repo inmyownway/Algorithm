ece1414da7eff0b35d4f85fb10a268b2c48b28e8
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-06-21 16:36:47 +0900

    [Gold II] Title: 쓰레기 치우기, Time: 104 ms, Memory: 12800 KB -BaekjoonHub

---
+++ 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] card, result;
+
+    static int N, M;
+    static int[][] board;
+    static int trashCnt;
+    static int robotCnt;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+
+                if (board[i][j] == 1) {
+                    trashCnt++;
+                }
+
+            }
+
+        }
+        while (trashCnt > 0) {
+
+            // 반복문 돌때마다 청소기 추가
+            robotCnt++;
+
+            if (board[0][0] == 1) {
+                board[0][0] = 0;
+                trashCnt--;
+            }
+            clean(0, 0);
+        }
+        System.out.println(robotCnt);
+    }
+
+    public static void clean(int x, int y) {
+        if (x == N - 1 && y == M - 1) {
+            if (board[x][y] == 1) {
+                board[0][0] = 0;
+                trashCnt--;
+            }
+            return;
+        }
+
+        for (int i = y; i < N; i++) {
+            for (int j = x; j < M; j++) {
+                if (board[i][j] == 1) {
+                    board[i][j] = 0;
+                    trashCnt--;
+                    clean(j, i);
+                    return;
+
+                }
+            }
+        }
+        return;
+    }
+}
