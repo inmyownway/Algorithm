760d7b50a267345523c71b52e002138a81d8888e
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-07-02 15:41:11 +0900

    [level 3] Title: 단속카메라, Time: 9.81 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,47 +1,31 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] routes) {
-        int result = 0;
-        
-        
-        Arrays.sort(routes,new Comparator<int[]>(){
+        int answer = 0;
+            
+        // Arrays.sort(routes, new Comparator<int[]>() {
+        //     @Override
+        //     public int compare(int[] route1, int[] route2) {
+        //         return route1[1] - route2[1];
+        //     }
+        // });
+           Arrays.sort(routes,new Comparator<int[]>(){
             @Override
-            public int compare(int[] a,int[] b)
-            {   
-            return Integer.compare(a[1],b[1]);
+            public int compare(int[] route1,int[] route2){
+                return route1[1]-route2[1];
             }
         });
-                    
-        boolean[] v= new boolean[routes.length];
+      
         
-        int l = routes.length;
-        int answer=0;
-        for(int i=0;i<l;i++)
+        int cam= Integer.MIN_VALUE;
+        
+        for(int i=0;i<routes.length;i++)
         {
-            
-            if(!v[i])
-            {
-                v[i]=true;
+            if(cam < routes[i][0]){
                 answer++;
-            
-                int num = routes[i][1];
-            
-                for(int j=0;j<l;j++)
-                {
-                    if(v[j]==false)
-                    {
-                        if(routes[j][0] <= num  && num<= routes[j][1])
-                        {
-                            v[j]=true;
-                        }
-                    }
-                }
+                cam=routes[i][1];
             }
-            
         }
         return answer;
     }
 }
-
-//
