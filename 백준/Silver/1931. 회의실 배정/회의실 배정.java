d6ea456e45cce483317b4c8e996f872a02145eb4
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-07-04 23:37:27 +0900

    [Silver I] Title: 회의실 배정, Time: 432 ms, Memory: 45032 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -13,21 +13,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine()); // 회의의 수 읽기
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
 
         arr = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            int s = Integer.parseInt(st.nextToken()); // 시작 시간
-            int e = Integer.parseInt(st.nextToken()); // 종료 시간
+            st = new StringTokenizer(bf.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
             arr.add(new int[]{s, e});
         }
 
-        // 종료 시간 기준으로 정렬, 종료 시간이 같으면 시작 시간으로 정렬
         Collections.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
+
                 if (o1[1] == o2[1]) {
                     return o1[0] - o2[0];
                 }
@@ -39,11 +41,12 @@ public class Main {
         int[] pre = arr.get(0);
         for (int i = 1; i < N; i++) {
             int[] now = arr.get(i);
-            if (pre[1] <= now[0]) { // 이전 회의의 종료 시간보다 현재 회의의 시작 시간이 같거나 늦다면
+            if (pre[1] <= now[0]) {
                 cnt++;
-                pre = now; // 현재 회의를 이전 회의로 설정
+                pre = now;
             }
+
         }
-        System.out.println(cnt); // 최대 사용할 수 있는 회의실의 수 출력
+        System.out.println(cnt);
     }
 }
