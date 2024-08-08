8f255f7d6e29a2e847d82686dce112f671b1ddcc
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-08-09 01:16:42 +0900

    [level 3] Title: 최고의 집합, Time: 0.34 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,24 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, int s) {
-        // 합이 n보다 작으면 -1 반환
-        if (s < n) {
+        int[] answer = new int[n];
+    
+        
+        int num = s/n;
+        int div = s%n;
+
+
+        if(n==1 && s==1)
+            return new int[]{-1};
+        if(s==1)
+            return new int[]{-1};
+        if(n==1)
+            return new int[]{-1};
+                if(n > s) {
             return new int[]{-1};
         }
         
-        int[] answer = new int[n];
-        int num = s / n; // 각 자리에 들어갈 기본 값
-        int div = s % n; // 나머지 값 (나머지만큼의 자리에는 1을 더해준다)
-        
-        for (int i = 0; i < n; i++) {
-            answer[i] = num;
+        for(int i=0;i<n;i++)
+        { 
+            int x=0;
+            if(i>=n-div)
+                x=1;
+            answer[i]=num+x;
         }
         
-        // 뒤쪽에서부터 div 개수 만큼 1을 더해줌
-        for (int i = 0; i < div; i++) {
-            answer[n - 1 - i]++;
-        }
+        
         
         return answer;
     }
-}
+}   
