c89412f4f0a839d56d9462939671ebbc7e43bb62
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-08-11 13:55:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.68 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,61 +1,34 @@
-import java.util.*;
 class Solution {
     static int[][] island;
     static int N;
-    static int[] parents;
-    
-    static void makeParents()
-    {
-        parents= new int[N];
-        for(int i=0;i<N;i++)
-        {
-            parents[i]=i;
-        }
-    }
-    static int find(int x)
-    {
-        if(x==parents[x])
-            return x;
-        
-        return parents[x]=find(parents[x]);
-    }
-    static void union(int a,int b)
-    {
-        int aRoot= find(a);
-        int bRoot= find(b);
-        
-        if(aRoot!=bRoot)
-            parents[bRoot]=aRoot;
-    
-    }
     public int solution(int n, int[][] costs) {
         int answer = 0;
         N=n;
-        
-        makeParents();
-        
-Arrays.sort(costs, new Comparator<int[]>() {
-    @Override
-    public int compare(int[] o1, int[] o2) {
-        return Integer.compare(o1[2], o2[2]);
-    }
-});
-
-        
-        for(int[] edge:costs)
+        island= new int[N][N];
+        for(int i=0;i<N;i++)
         {
-            int from= edge[0];
-            int to =edge[1];
-            int cost = edge[2];
-            
-            int fromParent = find(from);
-            int toParent = find(to);
-            
-            if(fromParent == toParent)
-                continue;
-            union(fromParent,toParent);
-            answer+=cost;
+        for(int j=0;j<N;j++)
+        {
+            island[i][j]=Integer.MAX_VALUE;
         }
+        
+        }
+        for(int[] cost:costs)
+        {
+            int a= cost[0];
+            int b= cost[1];
+            int c= cost[2];
+            
+            island[a][b]=Math.min(island[a][b],c);
+        }
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+            answer+=island[i][j];
+            }
+        }
+        
         return answer;
     }
 }
