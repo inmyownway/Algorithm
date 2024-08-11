f7240425a6e9ecb44258038a9f1d596aedd35e76
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-08-11 16:17:32 +0900

    [level 3] Title: 입국심사, Time: 180.21 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,35 +1,33 @@
+import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        
-    
-        long left=0;
-        long right = times[times.length-1]*(long)n;
-        
+        long answer=0;
+        Arrays.sort(times);
+        long left= 0;
+        long right= times[times.length-1]*(long)n;
+  
         while(left<=right)
         {
-
-            long mid = (left+right)/2;
-            long allTime=0;
+            long sum=0;
+            long mid = (left+ right)/2;
             
             for(int i=0;i<times.length;i++)
             {
-                allTime+=mid/times[i]; 
-            }
-            
-            if(allTime <n)
-            {
-
-                left = mid+1;
+                sum+= mid/times[i];
                 
             }
-            else 
-            {
-            right=mid-1;
-            }
             
-        }
+            if(sum<n)
+            {
         
-        return left;
+                left= mid+1;
+            }
+            else if(sum >=n)
+            {
+                right= mid-1;
+                answer= mid;
+            }
+        }
+        return answer;
     }
 }
