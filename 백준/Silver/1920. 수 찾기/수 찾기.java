57ad15085ac80dd79dd4f4fb146fba9daba50871
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-08-13 21:16:11 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 56920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,66 +1,62 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+    static int N, M;
+    static int[] A;
 
-        int n = in.nextInt();
+    public static void main(String[] args) throws IOException {
 
-        StringBuilder sb = new StringBuilder();
-        int[] arr =new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=in.nextInt();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        A = new int[N];
+    
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(A);
+        M = Integer.parseInt(bf.readLine());
 
-        int m = in.nextInt();
-
-        for(int i=0;i<m;i++)
-        {
-            if(binarySerach(arr,in.nextInt())>=0)
-            {
-                sb.append(1+"\n");
-
+        st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            boolean flag = binarySearch(Integer.parseInt(st.nextToken()));
+            if (flag) {
+                sb.append(1);
+            } else {
+                sb.append(0);
             }
-            else
-            {
-                sb.append(0+"\n");
+
+            if (i != M - 1) {
+                sb.append("\n");
             }
         }
         System.out.println(sb);
 
     }
-    public static int binarySerach(int[] arr,int target)
-    {
-        int low = 0;
-        int high= arr.length-1;
 
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
+    public static boolean binarySearch(int n) {
 
-            if (target<arr[mid])
-            {
-                high=mid-1;
+        int left = 0;
+        int right = A.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (A[mid] == n) {
+                return true;
             }
-            else if (target>arr[mid])
-            {
-                low=mid+1;
-
-            }
-            else{
-                return mid;
+            if (n > A[mid]) {
+                left = mid + 1;
+            } else if (n < A[mid]) {
+                right = mid - 1;
             }
         }
-        return -1;
-
-
+        return false;
     }
-
-
-
-
 }
