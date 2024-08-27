cb376de1e0631657cc773cc6724c240ff894ffd1
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-08-27 22:32:17 +0900

    [Silver I] Title: 볼 모으기, Time: 192 ms, Memory: 18056 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -1,63 +1,90 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     static int N;
-    static int[] arr;
-    static int answer= Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException{
-        
-        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(bf.readLine());
-        
-        
-        N= Integer.parseInt(st.nextToken());
-        arr= new int[N];
-        String str= bf.readLine();
-        for(int i=0;i<N;i++)
-        {
-            if(str.charAt(i)=='R')
-            {
-                arr[i]=1;
+    static int answer = Integer.MAX_VALUE;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        str = bf.readLine();
+
+        // 파란색 왼쪽으로
+        boolean flag;
+        int cnt = 0;
+
+        flag = false;
+        cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (!flag) {
+                if (str.charAt(i) == 'R') {
+                    flag = true;
+                }
+
             }
-            else{
-                arr[i]=0;
-            }
-        }
-        // 코드를 작성해주세요
-        
-        for(int i=0;i<=1;i++)
-        {
-            check(i);
-        }
-        System.out.println(answer);
-    }
-    public static void check(int ball)
-    {
-        // 0은 blue
-        
-        int lastIdx=N-1;
-        boolean all = false;
-        while(true){
-            if(lastIdx<0)
-            {
-                all=true;
-                break;
-            }
-            if(arr[lastIdx]!=ball)
-            {
-                break;
-            }
-            lastIdx--;
-        }  
-        int cnt=0;
-        for(int i=0;i<lastIdx;i++)
-        {
-            if(arr[i]==ball)
-            {
+            if (flag && str.charAt(i) == 'B') {
                 cnt++;
             }
         }
-        answer= Math.min(answer,cnt);
-        
+        answer = Math.min(answer, cnt);
+
+        // 빨간 왼쪽
+        flag = false;
+        cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (!flag) {
+                if (str.charAt(i) == 'B') {
+                    flag = true;
+                }
+
+            }
+            if (flag && str.charAt(i) == 'R') {
+                cnt++;
+            }
+        }
+        answer = Math.min(answer, cnt);
+
+        // 파란오른쪽
+        flag = false;
+        cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (!flag) {
+                if (str.charAt(i) == 'R') {
+                    flag = true;
+                }
+
+            }
+
+            if (flag && str.charAt(i) == 'B') {
+                cnt++;
+            }
+        }
+        answer = Math.min(answer, cnt);
+
+        // 빨간 오르쪽
+
+        flag = false;
+        cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (!flag) {
+                if (str.charAt(i) == 'B') {
+                    flag = true;
+                }
+
+            }
+            if (flag && str.charAt(i) == 'R') {
+                cnt++;
+            }
+        }
+        answer = Math.min(answer, cnt);
+
+        System.out.println(answer);
     }
+
+
 }
