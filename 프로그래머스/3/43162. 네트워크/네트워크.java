aa91e259382eaca09b7bb638c7e329821c39a6fe
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-09-01 23:51:51 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,41 +1,35 @@
 import java.util.*;
+import java.io.*;
 class Solution {
     static boolean[] v;
-    static int answer;
-    public int solution(int n, int[][] computers) {
-        answer = 0;
-        v= new boolean[n];
-        
-        for(int i=0;i<n;i++)
-        {
-            if(!v[i])
-            {
-                
+     public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        v = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (!v[i]) {
+
+                Queue<Integer> q = new LinkedList<>();
                 answer++;
-                v[i]=true;
-                Queue<Integer> q= new LinkedList<>();
+                v[i] = true;
                 q.add(i);
-                
-                while(!q.isEmpty())
-                {
-                    int node= q.poll();
-                    
-                    for(int j=0;j<n;j++)
-                    {
-                        if(computers[node][j]==1 && !v[j])
-                        {
-                            v[j]=true;
+
+                while (!q.isEmpty()) {
+                    int now = q.poll();
+
+ for (int j = 0; j < n; j++) {
+                        if (!v[j] && computers[now][j] == 1) {
                             q.add(j);
+                            v[j] = true;
                         }
                     }
-
                 }
-
             }
+
         }
-        
+
         return answer;
     }
-
-    
 }
