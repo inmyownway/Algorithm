38fa0eae3b26e510a49c2660ab81ab4675e01dcc
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-09-11 19:59:31 +0900

    [Gold V] Title: 수 고르기, Time: 276 ms, Memory: 28056 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,60 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N,M;
-    static ArrayList<Integer> arr;
-    static int answer=Integer.MAX_VALUE;
+    static int N, M;
+    static int[] arr;
+    static int answer = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        N= Integer.parseInt(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-        // N= 10 만
+        for (int i = 0; i < N; i++) {
 
+            arr[i] = Integer.parseInt(bf.readLine());
 
-        arr =new ArrayList<>();
-        for(int i=0;i<N;i++)
-        {
-            arr.add(Integer.parseInt(bf.readLine()));
         }
 
-
-        Collections.sort(arr);
-
-        int s=0;
-        int e=0;
-
+        Arrays.sort(arr);
+        int left = 0;
+        int right = 0;
+        int num = 0;
         // 1 2 3 4 5
-        //
-        while(e<N)
-        {
-            int r= arr.get(e)-arr.get(s);
 
-            if(r<M)
-            {
-                e++;
-            }
-            else if(r>M)
-            {
-                answer= Math.min(r,answer);
-                s++;
-            }else{
-                answer=r;
+        while (right < N) {
+
+            // System.out.println(left + " " + right);
+            num = arr[right] - arr[left];
+
+            if (num < M) {
+                right++;
+            } else if (num > M) {
+                answer = Math.min(answer, num);
+                left++;
+            } else {
+                answer = num;
                 break;
             }
 
+
         }
         System.out.println(answer);
-
+        // 코드를 작성해주세요
     }
-
 }
