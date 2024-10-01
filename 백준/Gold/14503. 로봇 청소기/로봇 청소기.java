e639584e663541334534e360cf3bf644c08d53f0
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-10-01 21:47:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,74 +1,116 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int x, y, d;
-    static int[][] board;
-    static boolean[][] visited;
 
-    static int[] dx = {-1, 0, 1, 0}; // 북, 동, 남, 서
+    static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static int N, M;
+    static int[][] board;
+    static boolean[][] isClean;
+    static int x, y, d;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        N = sc.nextInt();
-        M = sc.nextInt();
-        x = sc.nextInt();
-        y = sc.nextInt();
-        d = sc.nextInt();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         board = new int[N][M];
-        visited = new boolean[N][M];
-        visited[x][y] = true;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                board[i][j] = sc.nextInt();
-            }
-        }
-
-        while (true) {
-            boolean flag = false;
-
-            for (int i = 0; i < 4; i++) {
-                d = (d - 1 + 4) % 4;
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (!visited[nx][ny] && board[nx][ny] == 0) {
-                        flag = true;
-                        x = nx;
-                        y = ny;
-                        visited[x][y] = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!flag) {
-                int backX = x - dx[d];
-                int backY = y - dy[d];
-
-                if (backX < 0 || backX >= N || backY < 0 || backY >= M || board[backX][backY] == 1) {
-                    break;
-                } else {
-                    x = backX;
-                    y = backY;
-                }
-            }
-        }
-
+        isClean = new boolean[N][M];
         int answer = 0;
+        st = new StringTokenizer(bf.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 1) {
+                    isClean[i][j] = true;
+                }
+            }
+        }
+        int c = 0;
+        while (true) {
+            //  System.out.println(x + " " + y);
+            if (!isClean[x][y]) {
+                isClean[x][y] = true;
+
+            }
+
+            if (allCheck(x, y)) {
+                if (d == 0) {
+                    x += 1;
+                } else if (d == 1) {
+                    y -= 1;
+                } else if (d == 2) {
+                    x -= 1;
+                } else if (d == 3) {
+                    y += 1;
+                }
+
+                if (!isBoundary(x, y)) {
+                    break;
+                }
+                if (board[x][y] == 1) {
+                    break;
+                }
+                continue;
+            } else {
+
+                d -= 1;
+                if (d < 0) {
+                    d = 3;
+                }
+
+                if (isBoundary(x + dx[d], y + dy[d]) && !isClean[x + dx[d]][y + dy[d]]
+                        && board[x + dx[d]][y + dy[d]] != 1) {
+                    x += dx[d];
+                    y += dy[d];
+                }
+            }
+            c++;
+//            if (c == 10) {
+//                break;
+//            }
+
+        }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (visited[i][j]) {
+                if (isClean[i][j] && board[i][j] == 0) {
                     answer++;
                 }
             }
         }
-
         System.out.println(answer);
+
+
+    }
+
+    public static boolean allCheck(int x, int y) {
+        int cnt = 0;
+        for (int idx = 0; idx < 4; idx++) {
+            int nx = x + dx[idx];
+            int ny = y + dy[idx];
+
+            if (isBoundary(nx, ny) && isClean[nx][ny] == false && board[nx][ny] == 0) {
+                cnt++;
+            }
+        }
+
+        if (cnt >= 1) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean isBoundary(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
