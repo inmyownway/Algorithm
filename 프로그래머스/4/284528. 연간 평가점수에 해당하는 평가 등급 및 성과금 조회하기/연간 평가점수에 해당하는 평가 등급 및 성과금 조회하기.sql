b7d0b9fb2a015693fbd672ee84454f1ae734d54a
Jungmin <90558247+inmyownway@users.noreply.github.com>
2024-10-30 23:59:03 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,15 +1,19 @@
--- # 1
 
-select a.emp_no,a.emp_name,
-(case
-    when avg(score) >= 96 then 'S' 
-    when avg(score) >=90  then 'A'
-    when avg(score)  >= 80  then 'B' 
-    else 'C' end ) as GRADE ,
- (case 
-  when avg(score) >= 96  then a.sal*0.2 
-  when avg(score) >=90  then a.sal*0.15
-  when avg(score) >= 80 then a.sal*0.1 else 0 end) as BONUS
-from HR_employees a inner join hr_grade b on a.emp_no = b.emp_no 
-group by a.emp_no
-order by a.emp_no
+with cte as(
+select emp_no,avg(score) as score from hr_grade
+group by emp_no)
+
+
+
+select e.emp_no,e.emp_name,
+case when h.score >= 96 then 'S' 
+when h.score >= 90 and h.score < 96 then 'A'
+when h.score >= 80 and h.score < 90 then 'B'
+else 'C' end as grade,
+case when h.score >= 96 then e.sal*0.2
+when h.score >= 90 and h.score < 96 then e.sal*0.15
+when h.score >= 80 and h.score < 90 then e.sal*0.1
+else e.sal*0 end as bonus
+
+
+from HR_EMPLOYEES e join cte h on e.emp_no = h.emp_no
