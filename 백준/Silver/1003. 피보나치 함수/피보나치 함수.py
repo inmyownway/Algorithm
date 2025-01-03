6974ac84e4305049a826c9dff492cb85c9120acd
Jungmin <90558247+inmyownway@users.noreply.github.com>
2025-01-03 17:12:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,17 @@
+n = int(input())
+for j in range(0,n):
+    zero =1
+    temp =0
+    one = 0
+    n= int(input())
+    for i in range (0,n):
+        temp =one
+        one +=zero
+        zero=temp
+    print(zero,one)
+
+
+
+
+
+
